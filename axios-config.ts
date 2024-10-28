62105b844b928551565a971bdc52c57363ce4b04
pratikdevelop <pratikraut88895@gmail.com>
2024-10-28 23:59:05 +0530

    update api endpoints

--- axios-config.ts
+++ axios-config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosConfig = axios.create({
-    baseURL: 'http://54.224.249.251/api',
+    baseURL: 'https://vixol72czg.execute-api.us-east-1.amazonaws.com/api',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzEyM2JmNjJkMjZmZjMzNjM0NWYxN2UiLCJpYXQiOjE3Mjk1ODgwODl9.oZgPmU2hcGLNcrLoCUgKEgfzltijlnEP8Ep3KhTkpDc'
